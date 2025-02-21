a1fe3629134c95a372757328e92e997e7313ea6c
kamran <kamran.zhr64@gmail.com>
2025-02-21 00:10:40 -0800

    deploy to netlify

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const client = axios.create({
   baseURL: "http://localhost:3004",
+  baseURL2: "https://api.digikala.com/v2/product",
 });
 export async function getProducts() {
   const { data } = await client("/products");
